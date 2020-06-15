0d878187d8d69455998aec14d2dba73b6f94c45f
shu <1005281785@qq.com>
2020-06-15 20:56:45 +0800

    commit markdwn

---
+++ markdwn/js/b.js
@@ -0,0 +1,55 @@
+
+
+var n = 10
+function sum(n){
+  if( n > 1){
+    return 1
+  }
+  else{
+    return n * sum(n - 1)  
+  }
+}
+sum(10)
+
+
+var x= 1
+function f(){
+  var y = 2
+
+}
+
+
+
+class HashMap{
+  constructor(){
+    this.value = []
+    this.key = []
+    this.next = []
+  }
+
+  getValue(num){
+    var indx = this.key[num]
+    return this.value[indx] 
+  }
+
+  设计模式：单例、观察者模式、消费者模式，echarts, 数据库， 
+  计算机网络http， 服务器， java, c++,   算 法，  leetcode, 优化， 计划目标， 
+
+}
+
+function add(a, b){
+  return a + b
+}
+
+class map extends Array{
+  constructor(){
+    super()
+    this.arr = []
+    this.index = {}
+  }
+  
+  
+    
+
+}
+ 
